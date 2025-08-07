e029121d0890d5572b242ba5469956b02320fb05
PriyamTiwari-py <priyam11.tiwari2001@gmail.com>
2025-08-07 21:09:13 +0530

    Added FastAPI for model prediction

---
+++ src/app.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import mlflow
+import pandas as pd
+
+# Define input schema using Pydantic
+class HousingInput(BaseModel):
+    MedInc: float
+    HouseAge: float
+    AveRooms: float
+    AveBedrms: float
+    Population: float
+    AveOccup: float
+    Latitude: float
+    Longitude: float
+
+app = FastAPI()
+model_name = "DecisionTreeRegressor"  # Use the actual model name you registered
+
+@app.on_event("startup")
+def load_model():
+    global model
+    model = mlflow.sklearn.load_model(f"models:/{model_name}/1")
+
+@app.get("/")
+def home():
+    return {"message": "California Housing Price Predictor API is live!"}
+
+@app.post("/predict")
+def predict_price(data: HousingInput):
+    df = pd.DataFrame([data.dict()])
+    prediction = model.predict(df)[0]
+    return {"predicted_price": prediction}
