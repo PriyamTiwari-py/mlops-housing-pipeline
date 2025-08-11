238323ce64ee5ea706bbd5558e0cc256b36dcefc
2023ad05045 <2023ad05045@wilp.bits-pilani.ac.in>
2025-08-11 11:42:45 +0530

    Update app.py

--- src/app.py
+++ src/app.py
@@ -3,7 +3,7 @@ from pydantic import BaseModel
 import mlflow
 import pandas as pd
 
-#   Define input schema using Pydantic
+# Define input schema using Pydantic
 class HousingInput(BaseModel):
     MedInc: float
     HouseAge: float
@@ -14,20 +14,23 @@ class HousingInput(BaseModel):
     Latitude: float
     Longitude: float
 
+
 app = FastAPI()
 model_name = "DecisionTreeRegressor"  # Use the actual model name you registered
+
 
 @app.on_event("startup")
 def load_model():
     global model
-   model = mlflow.sklearn.load_model(
-    f"models:/{model_name}/1"
-)
+    model = mlflow.sklearn.load_model(
+        f"models:/{model_name}/1"
+    )
 
 
 @app.get("/")
 def home():
     return {"message": "California Housing Price Predictor API is live!"}
+
 
 @app.post("/predict")
 def predict_price(data: HousingInput):
